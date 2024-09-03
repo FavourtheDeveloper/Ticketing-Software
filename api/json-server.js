8cef1b227ba52782b239602f4a14464bc526d08a
FavourtheDev <favourthedeveloper@gmail.com>
2024-09-03 22:37:15 +0100

    Vercel json file

--- api/json-server.js
+++ api/json-server.js
@@ -1,13 +1,22 @@
-const jsonServer = require('json-server');
+import jsonServer from 'json-server';
+import { createServer } from 'http';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+// Convert file URL to path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Create the JSON Server
 const server = jsonServer.create();
-const router = jsonServer.router('events.json'); // Path to your db.json file
+const router = jsonServer.router(path.join(__dirname, 'events.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(3000, () => {
-    console.log('JSON Server is running');
-  });
-
-module.exports = server;
+// Listen on port 3000
+const port = 3000;
+createServer(server).listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
