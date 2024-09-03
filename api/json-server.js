3ed1f8c25bf8facb8491294ffc14669f5e061493
FavourtheDev <favourthedeveloper@gmail.com>
2024-09-04 00:39:50 +0100

    Ticket Generation

--- api/json-server.js
+++ api/json-server.js
@@ -3,11 +3,9 @@ import { createServer } from 'http';
 import { fileURLToPath } from 'url';
 import path from 'path';
 
-// Convert file URL to path
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// Create the JSON Server
 const server = jsonServer.create();
 const router = jsonServer.router(path.join(__dirname, 'events.json'));
 const middlewares = jsonServer.defaults();
@@ -15,8 +13,7 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use(router);
 
-// Listen on port 3000
-const port = 3000;
+const port = process.env.PORT || 3000;
 createServer(server).listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
