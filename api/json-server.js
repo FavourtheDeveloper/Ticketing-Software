62d4fc7d8813d98ed59d4a2a4b927eabc5906201
FavourtheDev <favourthedeveloper@gmail.com>
2024-09-04 10:28:01 +0100

    Vercel deployment

--- api/json-server.js
+++ api/json-server.js
@@ -7,18 +7,18 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const server = jsonServer.create();
-const router = jsonServer.router(path.join(__dirname, 'events.json'));
+const router = jsonServer.router(path.join(__dirname, '../events.json'));
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
 server.use(router);
 
-console.log('Serving data from:', path.join(__dirname, 'events.json'));
+console.log('Serving data from:', path.join(__dirname, '../events.json'));
 
 
 const port = process.env.PORT || 3000;
 createServer(server).listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
-  console.log('Serving data from:', path.join(__dirname, 'events.json'));
+  console.log('Serving data from:', path.join(__dirname, '../events.json'));
  
 });
