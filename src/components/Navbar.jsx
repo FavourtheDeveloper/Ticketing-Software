d96b5b1c95fe649db93b9a05020276643d95bc03
FavourtheDev <favourthedeveloper@gmail.com>
2024-09-04 13:22:44 +0100

    Navbar Responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,24 +1,45 @@
-import React from 'react'
-import Button from './Button'
-import { Link } from 'react-router-dom'
-import { FaSquareXTwitter } from "react-icons/fa6";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaTwitter } from "react-icons/fa";
 
 const Navbar = () => {
-  return (
-    <div className='flex flex-row justify-around p-7'>
-        <div className="img">
-            <h1 className="text-lg lg:text-6xl font-bold text-white text-center">KinVent</h1>
-        </div>
-        <div className="links">
-            <ul className='inline-flex text-yellow-500 space-x-5 font-bold p-5 lg:text-xl'>
-                <Link to={"/"}><li>Home</li></Link>
-                <Link to={"/events"}><li>Events</li></Link>
-                <Link to={"/tickets"}><li>My Tickets</li></Link>
-                <Link><FaSquareXTwitter size={30} /></Link>
-            </ul>
-        </div>
-    </div>
-  )
-}
+  const [isOpen, setIsOpen] = useState(false);
 
-export default Navbar
+  const toggleMenu = () => setIsOpen(!isOpen);
+
+  return (
+    <div className="bg-gray-800 p-4 lg:p-7">
+      <div className="container mx-auto flex justify-between items-center">
+        <div className="text-white text-lg lg:text-6xl font-bold">
+          <h1 className="text-center">KinVent</h1>
+        </div>
+        <a
+          href="https://twitter.com/favourthedev"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="lg:hidden text-yellow-500"
+        >
+          <FaTwitter size={30} />
+        </a>
+        <div className={`lg:flex hidden`}>
+          <ul className="flex sm:flex-col lg:flex-row lg:space-x-5 text-yellow-500 font-bold p-5 lg:text-xl">
+            <Link to={"/"}>
+              <li className="py-2 lg:py-0">Home</li>
+            </Link>
+            <Link to={"/events"}>
+              <li className="py-2 lg:py-0">Events</li>
+            </Link>
+            <Link to={"/tickets"}>
+              <li className="py-2 lg:py-0">My Tickets</li>
+            </Link>
+            <Link to="#">
+              <FaTwitter size={30} />
+            </Link>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
