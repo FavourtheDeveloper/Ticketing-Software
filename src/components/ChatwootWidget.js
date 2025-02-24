733555159386b02eb82c5195e97a042ebab59f3c
FavourtheDev <favourthedeveloper@gmail.com>
2025-02-24 17:46:04 +0100

    updated some chatbot detailsss

--- src/components/ChatwootWidget.js
+++ src/components/ChatwootWidget.js
@@ -11,14 +11,14 @@ const ChatwootWidget = () => {
     script.onload = () => {
       if (window.chatwootSDK) {
         window.chatwootSDK.run({
-          websiteToken: `${import.meta.env.VITE_WEBSITE_TOKEN}`,
+          websiteToken: "MDFAK1wzZKG4iirZoNB1G6Wu",
           baseUrl: BASE_URL,
           user: {
             identifier: "iglory13@gmail.com",
             name: "Glory Isaiah",
             email: "iglory13@gmail.com",
             avatar_url: "",
-            identifier_hash: `${import.meta.env.VITE_HASH}`, // Ensure it's prefixed correctly
+            identifier_hash: "bBjjr17djDGezNLGW7u2b89Z", // Ensure it's prefixed correctly
           },
         });
       } else {
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig, loadEnv } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-  // Load environment variables
-  const env = loadEnv(mode, process.cwd(), '');
-
-  return {
-    plugins: [react()],
-    define: {
-      'import.meta.env.VITE_HASH': JSON.stringify(env.VITE_HASH),
-      'import.meta.env.VITE_WEBSITE_TOKEN': JSON.stringify(env.VITE_WEBSITE_TOKEN),
-    },
-  };
-});
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+})
